080f6f61e443412de24f7905025376d43f72ce48
superwhiskers <whiskerdev@protonmail.com>
2024-09-02 22:03:43 -0500

    add assignment 1 + assignment 2

---
+++ assignment-2/in-class-08-28.js
@@ -0,0 +1,30 @@
+let name = "Name";
+let age = 22;
+let is_student = true;
+
+console.log(`name: ${name} (${typeof name}), age: ${age} (${typeof age}), is_student: ${is_student} (${typeof is_student})`);
+
+let one = 4;
+let two = 37;
+
+console.log(`operands: ${one}, ${two}, +: ${one + two}, -: ${one - two}, *: ${one * two}, /: ${one / two}`);
+
+let sentence = "this is a sentence i guess";
+
+console.log(`sentence: "${sentence}", length: ${sentence.length}, first character: ${sentence[0]}, last character: ${sentence[sentence.length - 1]}`);
+
+let negative_number = -29;
+
+console.log(`number: ${negative_number}, sqrt: ${Math.sqrt(negative_number)}, squared: ${Math.pow(negative_number, 2)}, abs: ${Math.abs(negative_number)}`);
+
+console.log(`operands: ${one}, ${two}, >: ${one > two}, =: ${one === two}, <: ${one < two}`);
+
+let bool_one = true;
+let bool_two = false;
+
+console.log(`operands: ${one}, ${two}, &&: ${one && two}, ||: ${one || two}, !: ${one}`);
+
+let first = "John";
+let last = "Doe";
+
+console.log(`Hello ${first} ${last}!`);
